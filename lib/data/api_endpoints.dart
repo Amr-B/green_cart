7d97bbe7c0ba24db4ed564facae80c4c19f6c692
Amr-B <amr.b4dev@gmail.com>
2025-08-26 18:06:31 +0300

    refactor: setup folders for MVVM architecture

--- lib/data/api_endpoints.dart
+++ lib/data/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndPoints {
-  static const String baseUrl = 'http://192.168.1.26:3000';
+  static const String baseUrl = 'http://192.168.5.107:3000';
   static const String newGroceries = '$baseUrl/groceries';
   static const String vegetables = '$baseUrl/vegetables';
   static const String fruits = '$baseUrl/fruits';
